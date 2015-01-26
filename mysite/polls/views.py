0f38eefc045c7b9eac700b106d034d5c23de331b
Michael Gilbert <drumz4jah@yahoo.com>
2015-01-25 18:45:21 -0700

    Added template language to index to try to display a list of polls. There currently aren't any!

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question, Choice, Person
+
 # Create your views here.
 def index(request):
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	
 	return render(request, 'index.html', {})
